c609fb5ac45d1d7da998e721e9d11e6e2c29838a
lenz90 <lenz.90@gmail.com>
2019-10-05 23:30:36 -0500

    Se inicia el proyecto

---
+++ src/main/java/com/intercorp/lizardo/intercorpclientes/IntercorpClientesApplication.java
@@ -0,0 +1,13 @@
+package com.intercorp.lizardo.intercorpclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntercorpClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntercorpClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intercorp/lizardo/intercorpclientes/IntercorpClientesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.intercorp.lizardo.intercorpclientes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntercorpClientesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
